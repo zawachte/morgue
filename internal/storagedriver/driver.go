7ddd89ea9ad03476527a5bba355c37348b92881d
zach <zwachtel11@gmail.com>
2022-08-08 12:19:14 -0400

    move session to object

--- internal/storagedriver/driver.go
+++ internal/storagedriver/driver.go
@@ -35,6 +35,7 @@ type s3StorageDriver struct {
 	localStorageLocation string
 	region               string
 	bucket               string
+	awsSession           *session.Session
 	logger               zap.Logger
 }
 
@@ -43,12 +44,6 @@ func (l *s3StorageDriver) GetLocalStorageLocation() string {
 }
 
 func (l *s3StorageDriver) UploadTar(directoryName string) error {
-
-	// Create a single AWS session (we can re use this if we're uploading many files)
-	s, err := session.NewSession(&aws.Config{Region: aws.String(l.region)})
-	if err != nil {
-		return err
-	}
 
 	latestBackup := path.Join(l.localStorageLocation, directoryName)
 
@@ -59,19 +54,12 @@ func (l *s3StorageDriver) UploadTar(directoryName string) error {
 	}
 	defer file.Close()
 
-	// Get file size and read the file content into a buffer
 	fileInfo, _ := file.Stat()
 	var size int64 = fileInfo.Size()
 	buffer := make([]byte, size)
 	file.Read(buffer)
 
-	_, err = s3.New(s).ListBuckets(&s3.ListBucketsInput{})
-	if err != nil {
-		return err
-	}
-	// Config settings: this is where you choose the bucket, filename, content-type etc.
-	// of the file you're uploading.
-	_, err = s3.New(s).PutObject(&s3.PutObjectInput{
+	_, err = s3.New(l.awsSession).PutObject(&s3.PutObjectInput{
 		Bucket:             aws.String(l.bucket),
 		Key:                aws.String(directoryName),
 		Body:               bytes.NewReader(buffer),
@@ -96,10 +84,17 @@ type StorageDriverParams struct {
 
 func NewStorageDriver(params StorageDriverParams) (StorageDriver, error) {
 	if params.S3StorageDriverParams != nil {
+		sess, err := session.NewSession(&aws.Config{
+			Region: aws.String(params.S3StorageDriverParams.Region)})
+		if err != nil {
+			return nil, err
+		}
+
 		return &s3StorageDriver{
 			region:               params.S3StorageDriverParams.Region,
 			bucket:               params.S3StorageDriverParams.Bucket,
 			localStorageLocation: params.LocalStorageLocation,
+			awsSession:           sess,
 		}, nil
 	}
 
